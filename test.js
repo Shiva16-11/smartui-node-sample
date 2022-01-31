673b1bf04638cf206f72b696605ba33a05c6fb8d
Shiva16-11 <67579051+Shiva16-11@users.noreply.github.com>
2022-01-31 10:34:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 try {
   if (fs.existsSync("GITHUB_ENV")) {
+    console.log("file exists");
     //file exists
   }
 } catch(err) {
+  console.log("file does not");
   console.error(err)
 }

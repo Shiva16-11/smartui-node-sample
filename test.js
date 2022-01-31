453c1a206ec812942eefda69f4cc6ad3f9dc34f8
Shiva16-11 <67579051+Shiva16-11@users.noreply.github.com>
2022-01-31 12:17:51 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 const github = require('@actions/github');
 console.log("data from client payload", github.event.client_payload);
-console.log("------------");
+console.log("------------........");
 console.log("created check run context", github.context);
